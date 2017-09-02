c9910ba331b6709f4646b94acf180789432d62df
tk1to <tk02dr@gmail.com>
2017-09-02 18:07:23 +0900

    p 3

---
+++ problem_0003/ruby/solution.rb
@@ -0,0 +1,14 @@
+n = 600_851_475_143
+m = 3
+primes = [m]
+while 1 < n
+  n /= m if n % m == 0
+  while 1 < n
+    m += 2
+    if primes.all? {|p| 0 < m % p}
+      primes << m
+      break
+    end
+  end
+end
+puts primes[-1]
