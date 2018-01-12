17db1b12e43a5b0a9e93848c8637f7b6bae3a437
tk1to <tk02dr@gmail.com>
2018-01-12 15:18:12 +0900

    Add & edit problem0001 solve

--- problem_0001/elixir/main.exs
+++ problem_0001/elixir/main.exs
@@ -1,13 +1,10 @@
 defmodule Problem0001 do
   def calculate(limit) do
     Enum.filter(1..(limit - 1), fn(n) ->
-      ((rem(n, 3) == 0) || (rem(n, 5) == 0))
+      (rem(n, 3) == 0) || (rem(n, 5) == 0)
     end) |> Enum.reduce(fn(n, sum) ->
       n + sum
     end)
   end
-  def print do
-    IO.puts calculate(1000)
-  end
 end
-Problem0001.print
+IO.puts Problem0001.calculate(1000)
---
+++ problem_0001/ruby/main2.rb
@@ -0,0 +1,8 @@
+class Problem0001
+  def self.calculate(limit)
+    (1..(limit - 1)).select{|n|
+      n % 3 == 0 || n % 5 == 0
+    }.inject(:+)
+  end
+end
+puts Problem0001.calculate(1000)
