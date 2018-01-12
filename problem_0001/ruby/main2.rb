83c004ddf97ed02ee071d6277944a66acd11f488
tk1to <tk02dr@gmail.com>
2018-01-12 16:37:09 +0900

    Inject

--- problem_0001/ruby/main2.rb
+++ problem_0001/ruby/main2.rb
@@ -2,7 +2,9 @@ class Problem0001
   def self.calculate(limit)
     (1..(limit - 1)).select{|n|
       n % 3 == 0 || n % 5 == 0
-    }.inject(:+)
+    }.inject{|sum, n|
+      sum + n
+    }
   end
 end
 puts Problem0001.calculate(1000)
