1f7e194f458f890694f795d06471effa557783d0
tk1to <tk02dr@gmail.com>
2018-01-12 14:55:35 +0900

    Remove module from problem0001byelixir

--- problem_0001/elixir/main.exs
+++ problem_0001/elixir/main.exs
@@ -1,13 +1,5 @@
-defmodule Problem0001 do
-  def calculate(limit) do
-    Enum.filter(1..(limit - 1), fn(n) ->
-      ((rem(n, 3) == 0) || (rem(n, 5) == 0))
-    end) |> Enum.reduce(fn(n, sum) ->
-      n + sum
-    end)
-  end
-  def print do
-    IO.puts calculate(1000)
-  end
-end
-Problem0001.print
+Enum.filter(1..(1000 - 1), fn(n) ->
+  ((rem(n, 3) == 0) || (rem(n, 5) == 0))
+end) |> Enum.reduce(fn(n, sum) ->
+  n + sum
+end) |> IO.puts
