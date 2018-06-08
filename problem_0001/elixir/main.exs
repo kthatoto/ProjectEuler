d0f478d6d791f31d118e53b671df9fcd19a97b72
kthatoto <kthatoto@gmail.com>
2018-06-08 16:08:39 +0900

    Edited elixir 0001

--- problem_0001/elixir/main.exs
+++ problem_0001/elixir/main.exs
@@ -1,10 +1,5 @@
-defmodule Problem0001 do
-  def calculate(limit) do
-    Enum.filter(1..(limit - 1), fn(n) ->
-      (rem(n, 3) == 0) || (rem(n, 5) == 0)
-    end) |> Enum.reduce(fn(n, sum) ->
-      n + sum
-    end)
-  end
-end
-IO.puts Problem0001.calculate(1000)
+IO.puts Enum.filter(1..(1000 - 1), fn(n) ->
+  (rem(n, 3) == 0) || (rem(n, 5) == 0)
+end) |> Enum.reduce(fn(n, sum) ->
+  n + sum
+end)
