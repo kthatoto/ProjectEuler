98e19595a96c7b374130b16933c88a94944b6a34
kthatoto <kthatoto@gmail.com>
2018-06-09 18:49:33 +0900

    Updated no3 with go

--- problem_0003/go/main.go
+++ problem_0003/go/main.go
@@ -7,5 +7,17 @@ import (
 func main() {
   n := 600851475143
   m := 3
-  primes := []
+  for {
+    for {
+      if n % m != 0 {
+        break
+      }
+      n /= m
+    }
+    if n == 1 {
+      break
+    }
+    m += 2
+  }
+  fmt.Println(m)
 }
