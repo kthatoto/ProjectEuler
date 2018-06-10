f3dd20f1e4d807ffc08f9576c3b99d6bb72f90c7
kthatoto <kthatoto@gmail.com>
2018-06-10 21:24:57 +0900

    Added constant no1 with elixir

--- problem_0001/elixir/main.exs
+++ problem_0001/elixir/main.exs
@@ -1,5 +1,6 @@
-IO.puts Enum.filter(1..(1000 - 1), fn(n) ->
-  (rem(n, 3) == 0) || (rem(n, 5) == 0)
-end) |> Enum.reduce(fn(n, sum) ->
-  n + sum
+n = 1000
+IO.puts Enum.filter(1..(n - 1), fn(m) ->
+  (rem(m, 3) == 0) || (rem(m, 5) == 0)
+end) |> Enum.reduce(fn(m, sum) ->
+  m + sum
 end)
