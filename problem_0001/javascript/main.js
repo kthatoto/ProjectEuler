b6d832cb3fbaea1db75321cb06ffdc25a1a69f43
kthatoto <kthatoto@gmail.com>
2018-06-10 21:26:09 +0900

    Added constant no1 with js

--- problem_0001/javascript/main.js
+++ problem_0001/javascript/main.js
@@ -1,3 +1,3 @@
-let sum = 0
-for (let i = 1; i < 1000; i++) if (i % 3 == 0 || i % 5 == 0) sum += i
+let sum = 0, n = 1000
+for (let i = 1; i < n; i++) if (i % 3 == 0 || i % 5 == 0) sum += i
 console.log(sum)
