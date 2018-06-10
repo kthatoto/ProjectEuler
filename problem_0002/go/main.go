ebefb591051b0db80ca2df514062dc612e127396
kthatoto <kthatoto@gmail.com>
2018-06-10 22:04:38 +0900

    According to rule in no2

--- problem_0002/go/main.go
+++ problem_0002/go/main.go
@@ -5,16 +5,17 @@ import (
 )
 
 func main() {
+  max := 4000000
   sum := 0
   n0, n1 := 1, 2
   for {
     if n1%2 == 0 {
       sum += n1
     }
-    if n0 + n1 > 4000000 {
+    if n0 + n1 > max {
       break
     }
     n0, n1 = n1, n0 + n1
   }
-  fmt.Println(sum)
+  fmt.Print(sum)
 }
--- problem_0002/ruby/main.rb
+++ problem_0002/ruby/main.rb
@@ -1,3 +1,4 @@
+max = 4_000_000
 ary = [1, 2]
-ary << ary[-1] + ary[-2] while ary[-1] + ary[-2] <= 4_000_000
-puts ary.select(&:even?).inject(:+)
+ary << ary[-1] + ary[-2] while ary[-1] + ary[-2] <= max
+print ary.select(&:even?).inject(:+)
