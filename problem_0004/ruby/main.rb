b871f3f58d3d7c257a0a9446ebccc7b8b5f05439
kthatoto <kthatoto@gmail.com>
2018-06-18 18:48:32 +0900

    Used def no4 in ruby

--- problem_0004/ruby/main.rb
+++ problem_0004/ruby/main.rb
@@ -1,16 +1,17 @@
-digit = 3
-min = 10 ** (digit - 1)
-max = (10 ** digit) - 1
-found = false
-(max ** 2).downto(min ** 2) do |n|
-  if n.to_s === n.to_s.reverse
-    (min..max).each do |m|
-      if n % m === 0 && (min <= (l = (n / m)) && l <= max)
-        print n
-        found = true
-        break
+
+def solve(digit)
+  min = 10 ** (digit - 1)
+  max = (10 ** digit) - 1
+  found = false
+  (max ** 2).downto(min ** 2) do |n|
+    if n.to_s === n.to_s.reverse
+      (min..max).each do |m|
+        if n % m === 0 && (min <= (l = (n / m)) && l <= max)
+          return n
+        end
       end
+      break if found
     end
-    break if found
   end
 end
+print solve(3)
--- problem_0004/ruby/main2.rb
+++ problem_0004/ruby/main2.rb
@@ -1,15 +1,14 @@
-digit = 3
-min = 10 ** (digit - 1)
-max = (10 ** digit) - 1
-catch :break do
+def solve(digit)
+  min = 10 ** (digit - 1)
+  max = (10 ** digit) - 1
   (max ** 2).downto(min ** 2) do |n|
     next if n.to_s != n.to_s.reverse
     (min..max).each do |m|
       l = n / m
       if n % m === 0 && min <= l && l <= max
-        print n
-        throw :break
+        return n
       end
     end
   end
 end
+print solve(3)
